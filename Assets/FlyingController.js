06cf1bd21978f24a136e880a36ea6b47689e3ada
Sathvik Swamy <srswamy@uwaterloo.ca>
2015-10-18 04:24:27 -0400

    Added terrain, flight and flight controls.

---
+++ Assets/FlyingController.js
@@ -0,0 +1,37 @@
+﻿var rotateSpeed = 25.0;
+var speed = 50.0;
+ 
+function Update() {
+var transAmount = speed * Time.deltaTime;
+var rotateAmount = rotateSpeed * Time.deltaTime;
+ 
+if (Input.GetKey("up")) {
+transform.Rotate(rotateAmount, 0, 0);	
+}
+if (Input.GetKey("down")) {
+transform.Rotate(-rotateAmount, 0, 0);	
+}
+if (Input.GetKey("left")) {
+transform.Rotate(0, -rotateAmount, 0);
+}
+if (Input.GetKey("right")) {
+transform.Rotate(0, rotateAmount, 0);
+}
+ 
+if (Input.GetKey ("z")) {
+transform.Rotate(0, 0, rotateAmount);
+}
+        
+if (Input.GetKey ("x")) {
+transform.Rotate(0, 0, -rotateAmount);
+}
+ 
+if (Input.GetKey ("a")) {
+transform.Translate(0, 0, transAmount);
+}
+        
+if (Input.GetKey ("q")) {
+transform.Translate(0, 0, (transAmount * 2));
+}
+ 
+}
