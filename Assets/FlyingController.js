129805463f537e81c8102682fa57307d436ab2fd
Sathvik Swamy <srswamy@uwaterloo.ca>
2015-10-18 07:07:08 -0400

    Minor flight control changes.

--- Assets/FlyingController.js
+++ Assets/FlyingController.js
@@ -2,36 +2,31 @@
 var speed = 50.0;
  
 function Update() {
-var transAmount = speed * Time.deltaTime;
-var rotateAmount = rotateSpeed * Time.deltaTime;
+	var transAmount = speed * Time.deltaTime;
+	var rotateAmount = rotateSpeed * Time.deltaTime;
  
-if (Input.GetKey("up")) {
-transform.Rotate(rotateAmount, 0, 0);	
-}
-if (Input.GetKey("down")) {
-transform.Rotate(-rotateAmount, 0, 0);	
-}
-if (Input.GetKey("left")) {
-transform.Rotate(0, -rotateAmount, 0);
-}
-if (Input.GetKey("right")) {
-transform.Rotate(0, rotateAmount, 0);
-}
- 
-if (Input.GetKey ("z")) {
-transform.Rotate(0, 0, rotateAmount);
-}
-        
-if (Input.GetKey ("x")) {
-transform.Rotate(0, 0, -rotateAmount);
-}
- 
-if (Input.GetKey ("a")) {
-transform.Translate(0, 0, transAmount);
-}
-        
-if (Input.GetKey ("q")) {
-transform.Translate(0, 0, (transAmount * 2));
-}
+	if (Input.GetKey("down")) {
+		transform.Rotate(rotateAmount, 0, 0);	
+	}
+
+	if (Input.GetKey("up")) {
+		transform.Rotate(-rotateAmount, 0, 0);	
+	}
+
+	if (Input.GetKey("left")) {
+		transform.Rotate(0, -rotateAmount, 0);
+	}
+
+	if (Input.GetKey("right")) {
+		transform.Rotate(0, rotateAmount, 0);
+	}
+	 
+	if (Input.GetKey ("a")) {
+		transform.Translate(0, 0, transAmount);
+	}
+			
+	if (Input.GetKey ("q")) {
+		transform.Translate(0, 0, (transAmount * 2));
+	}
  
 }
